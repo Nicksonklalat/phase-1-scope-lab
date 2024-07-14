4e034bfbe259860346953ac16bc66e04c10e58ff
Nickson <lalatnickson@gmail.com>
2024-07-14 12:21:51 +0300

    Scope Lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+
+// Write your solution in this file!
+var customerName = "bob"
+function upperCaseCustomerName(){
+    window.customerName = customerName.toUpperCase()
+}
+function setBestCustomer(){
+    let bestCustomer = "not bob"
+    window.bestCustomer = bestCustomer
+}
+function overwriteBestCustomer(){
+    let overwrites = "maybe bob"
+    window.bestCustomer = overwrites
+}
+const least = "Alice"
+function changeLeastFavoriteCustomer(){
+    least = "bob"
+}
